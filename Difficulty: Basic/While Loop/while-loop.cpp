b6a9d46b8da38899e76e7b0495391066e54c54d9
AdilMansoori499 <adilmansuri499@gmail.com>
2026-07-01 09:34:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void utility(int x) {
+          //cout<<"enter the value of x"<<endl;
+  //  cin>>x;
+    while(x>=0)
+    {
+        cout<<x<<" ";
+        x--;
+    }
+    
+        // code here
+        
+    }
+};
