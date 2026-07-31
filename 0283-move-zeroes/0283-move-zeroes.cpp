bddeda227c32a9414211f02767d1cb6aff3a5f1a
AdilMansoori499 <adilmansuri499@gmail.com>
2026-07-31 23:22:29 +0530

    Time: 3 ms (14.56%), Space: 23.8 MB (56.68%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0; // Pointer for the last non-zero element position
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
