97db85fcc5aadf8237dae740d1311f01cd4c6bc4
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-02 19:20:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.cpp
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -1,21 +1,29 @@
 class Solution {
   public:
     void rotateArr(vector<int>& arr, int d) {
+        // int n=arr.size();
+        // d=d%n;
+        // vector<int> temp;
+        // for(int i=0;i<d;i++)
+        // {
+        //     temp.push_back(arr[i]);
+        // }
+        // for(int i=d;i<n;i++)
+        // {
+        //     arr[i-d]=arr[i];
+        // }
+        // for(int i=n-d;i<n;i++)
+        // {
+        //     arr[i]=temp[i-(n-d)];
+        // }
+        
+        
+        // optimal solution
+        
         int n=arr.size();
         d=d%n;
-        vector<int> temp;
-        for(int i=0;i<d;i++)
-        {
-            temp.push_back(arr[i]);
-        }
-        for(int i=d;i<n;i++)
-        {
-            arr[i-d]=arr[i];
-        }
-        for(int i=n-d;i<n;i++)
-        {
-            arr[i]=temp[i-(n-d)];
-        }
-        
+        reverse(arr.begin(), arr.begin() + d);
+        reverse(arr.begin() + d,arr.begin() + n);
+        reverse(arr.begin(), arr.begin() + n);
     }
 };
