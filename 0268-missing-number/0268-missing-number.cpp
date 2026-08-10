739a7779c109871c09f203ed9c576496e125eb92
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-10 23:12:01 +0530

    Time: 0 ms (100%), Space: 21.8 MB (39.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,25 +3,55 @@ public:
     int missingNumber(vector<int>& nums) {
 
         //brute approach
-        int n=nums.size();
-        int temp=0;
-        for(int i=0;i<=n;i++)
-        {
-            int flag=0;
-            for(int j=0;j<=n-1;j++)
-            {
-               if(nums[j]==i) 
-               {
-                flag=1;
-                break;
-               }
+        // int n=nums.size();
+        // int temp=0;
+        // for(int i=0;i<=n;i++)
+        // {
+        //     int flag=0;
+        //     for(int j=0;j<=n-1;j++)
+        //     {
+        //        if(nums[j]==i) 
+        //        {
+        //         flag=1;
+        //         break;
+        //        }
             
-            }
-            if(flag==0)
-               temp=i;
-        }
-        return temp;
+        //     }
+        //     if(flag==0)
+        //        temp=i;
+        // }
+        // return temp;
         
+
+       // optimal1 solution sum
+    //    int n=nums.size();
+    //    int sum=(n*(n+1))/2;
+    //    int sum1=0;
+    //    int temp=0;
+    //    for(int i=0;i<n;i++)
+    //    {
+    //     sum1+=nums[i];
+    //    }
+    //    temp=sum-sum1;
+    //    return temp;
+
+
+   // optimal2 solution xor
+   int n=nums.size();
+   int xor1=0;
+   for(int i=0;i<=n;i++)
+   {
+    xor1=xor1^i;
+   }
+   int xor2=0;
+   for(int i=0;i<n;i++)
+    {
+        xor2=xor2^nums[i];
+    }
+    return xor1^xor2;
+
+
+
         
         
     }
