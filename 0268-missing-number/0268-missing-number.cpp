87e61c3ea42545ed376a0044bd413ad7e4c63f6f
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-10 22:36:37 +0530

    Time: 543 ms (5.01%), Space: 21.8 MB (39.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int xorVal = n; // Initialize with n
-        
-        for (int i = 0; i < n; i++) {
-            xorVal ^= i ^ nums[i];
+
+        //brute approach
+        int n=nums.size();
+        int temp=0;
+        for(int i=0;i<=n;i++)
+        {
+            int flag=0;
+            for(int j=0;j<=n-1;j++)
+            {
+               if(nums[j]==i) 
+               {
+                flag=1;
+                break;
+               }
+            
+            }
+            if(flag==0)
+               temp=i;
         }
+        return temp;
         
-        return xorVal;
+        
+        
     }
 };
