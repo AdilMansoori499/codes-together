fb67b0ebf3546534e04c39660ed4e85e37924fac
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-24 23:59:10 +0530

    Time: 1654 ms (19.8%), Space: 34.5 MB (81.21%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,7 +3,7 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
 
-        //brute solution
+     //   brute solution
         int sum=0;
         int ans=0;
         for(int i=0;i<n;i++)
@@ -24,8 +24,27 @@ public:
             }
         }
         return ans;
-        
 
-        
+
+        //better solution
+        // map<long long,int> presummap;
+        // long long sum=0;
+        // int ans=0;
+        // for(int i;i<n;i++)  
+        // {
+        //     sum+=nums[i];
+        //     if(sum==k)
+        //     {
+        //         ans++;
+        //     }
+        //     int rem=sum-k;
+        //     if(presummap.find(rem)!=presummap.end())
+        //     {
+        //         sum+=nums[i];
+        //         ans++;
+        //     }
+        // }      
+
+        // return ans;
     }
 };
