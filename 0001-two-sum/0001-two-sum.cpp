effb68330485575668ffcbb9fea0b085685925ec
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-28 22:16:15 +0530

    Time: 6 ms (42.37%), Space: 15.2 MB (8.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,20 +3,38 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //brute solution
         int n=nums.size();
-        vector<int> arr;
+//         vector<int> arr;
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//  //fix sum=nums[i]+nums[j] with direct in if condition
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     arr.push_back(i);
+//                     arr.push_back(j);
+//                 }
+//             }
+//         }
+//         return arr;
+
+
+
+
+        //optimal solution using hash
+        map<int,int> mpp;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            int a=nums[i];
+            int more=target-a;
+            if(mpp.find(more)!=mpp.end())
             {
- //fix sum=nums[i]+nums[j] with direct in if condition
-                if(nums[i]+nums[j]==target)
-                {
-                    arr.push_back(i);
-                    arr.push_back(j);
-                }
+                return{mpp[more],i};
             }
+            mpp[a]=i;
         }
-        return arr;
+        return{-1,-1};
+
         
     }
 };
