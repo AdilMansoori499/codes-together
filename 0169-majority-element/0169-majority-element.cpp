13ab989cfbd846d656149d563a9991151d8f2833
AdilMansoori499 <adilmansuri499@gmail.com>
2026-08-30 12:54:10 +0530

    Time: 0 ms (100%), Space: 28.2 MB (44.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,77 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, count = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+        //brute solution
+        int n=nums.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     int count=0;
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(nums[j]==nums[i])
+        //         {
+        //             count++;
+        //         }
+        //          if(count> n/2)
+        //     {
+        //         return nums[i];
+        //     }
+        //     }
+            
+        // }
+        // return -1;
+
+
+        //better solution using hashing
+        //  map<int,int> mpp;
+        //  for(int i=0;i<n;i++)
+        //  {
+        //     mpp[nums[i]]++;
+        //  }
+        //  for(auto it: mpp)
+        //  {
+        //     if(it.second>n/2)
+        //     {
+        //         return it.first;
+        //     }
+        //  }
+            
+        //  return -1;
+
+
+        //optimal solution moore voting algo
+
+        int count=0;
+        int el;
+        for(int i=0;i<n;i++)
+        {
+            if(count==0)
+            {
+                count=1;
+                el=nums[i];
             }
-            count += (num == candidate) ? 1 : -1;
+            else if(nums[i]==el)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
         }
-        return candidate;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==el)
+            {
+                cnt++;
+            }
+            if(cnt>n/2)
+            {
+                return el;
+            }
+        }
+
+        return -1;
     }
 };
