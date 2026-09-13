4273667f705c597ca21c4c165ecde0278443e5b0
AdilMansoori499 <adilmansuri499@gmail.com>
2026-09-13 12:51:04 +0530

    Time: 0 ms (100%), Space: 10 MB (93.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         //brute solution
+        // int n=matrix.size();
+        //   vector<vector<int>> ans(n , vector<int>(n));
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //        ans[j][n-1-i]=matrix[i][j];
+        //     }
+        // }
+        // matrix = ans;
+
+
+        //optimal solution
         int n=matrix.size();
-          vector<vector<int>> ans(n , vector<int>(n));
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
-               ans[j][n-1-i]=matrix[i][j];
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        matrix = ans;
+        for(int i=0;i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
